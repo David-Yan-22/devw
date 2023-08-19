1bf2182d8d9e4fbe9cb4a89bb3e16eb9f09bf3ee
David-Yan-22 <73031516+David-Yan-22@users.noreply.github.com>
2023-08-19 23:28:13 +1000

    mongoDB server

--- server.py
+++ server.py
@@ -6,10 +6,13 @@ from bson import json_util
 app = Flask(__name__)
 CORS(app)
 
+# Connect the MongoDB 
 client = pymongo.MongoClient("mongodb+srv://devw:devw123456@cluster0.vp7tf8d.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database("note-database")
 note_collection = pymongo.collection.Collection(db, "note")
 
+
+# Add a new note
 def add_notes(title, content, date):
     note = {"title": title, "content": content, "date": date}
     return note_collection.insert_one(note)
@@ -17,11 +20,12 @@ def add_notes(title, content, date):
 @app.route('/addnote', methods=['POST'])
 def api_post_note():
     try:
-        add_recipe(request.json.get('title'), request.json.get('content'), request.json.get('date'))
+        add_notes(request.json.get('title'), request.json.get('content'), request.json.get('date'))
         return jsonify({'Success': "Yay"})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
+# index page, list all notes
 @app.route('/listnotes', methods=['GET'])
 def api_get_notes():
     try:
@@ -30,9 +34,15 @@ def api_get_notes():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-@app.route('/editnote/<int:id>')
+
+# Edit note
+@app.route('/editnote/<int:id>', methods=['POST', 'GET'])
 def editnote(id):
-    return ""
+    if request.method == 'GET':
+        return ""
+    
+    if request.method == 'POST':
+        return ""
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
