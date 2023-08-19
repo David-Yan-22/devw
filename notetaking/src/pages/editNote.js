262cfd5717904c3edd0e15eb4d7f5570e3694559
Endangered358 <141904084+Endangered358@users.noreply.github.com>
2023-08-19 21:32:35 +1000

    creation of editNote.js and altering of index so that it runs

--- notetaking/src/pages/editNote.js
+++ notetaking/src/pages/editNote.js
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function editNote() {
   return (
     <div>
-        <h1>slfksjdlkfj</h1>
+    <h1>sadfsd</h1>
     </div>
   )
 }
