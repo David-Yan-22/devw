8a3e7a58065d3f1db3a5d5733a1e1f8e179499d9
Endangered358 <141904084+Endangered358@users.noreply.github.com>
2023-08-19 21:41:08 +1000

    creation of components folder + change to globals to make stuff readable

--- notetaking/src/pages/editNote.js
+++ notetaking/src/pages/editNote.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Title1 from "../components/Title1.jsx"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function editNote() {
   return (
     <div>
+    <Title1/>
     <h1>sadfsd</h1>
     </div>
   )
